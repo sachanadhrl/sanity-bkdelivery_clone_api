1e17448a2d51780c091177d38e1230ef64628de9
unknown <sachanadharul@ymail.com>
2023-02-11 12:50:57 +0700

    feat: bootstrap sanity studio

---
+++ sanity.cli.js
@@ -0,0 +1,8 @@
+import {defineCliConfig} from 'sanity/cli'
+
+export default defineCliConfig({
+  api: {
+    projectId: '6nxiri9i',
+    dataset: 'production'
+  }
+})
---
+++ sanity.config.js
@@ -0,0 +1,18 @@
+import {defineConfig} from 'sanity'
+import {deskTool} from 'sanity/desk'
+import {visionTool} from '@sanity/vision'
+import {schemaTypes} from './schemas'
+
+export default defineConfig({
+  name: 'default',
+  title: 'bkdelivery_clone',
+
+  projectId: '6nxiri9i',
+  dataset: 'production',
+
+  plugins: [deskTool(), visionTool()],
+
+  schema: {
+    types: schemaTypes,
+  },
+})
